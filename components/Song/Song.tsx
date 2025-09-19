8be301c36371002a682901dac11a5f828460c00c
Ramey Robles <rameyrobo@gmail.com>
2025-09-19 04:50:33 -0700

    Added animation styles and adjusted height to spotify player

--- components/Song/Song.tsx
+++ components/Song/Song.tsx
@@ -5,11 +5,12 @@ export default function Song() {
       style={{ borderRadius: 12 }}
       src="https://open.spotify.com/embed/album/7kUuNjLxJYEtUNm0KdrDBv?utm_source=generator"
       width="100%"
-      height="352"
+      height="152"
       frameBorder="0"
       allowFullScreen
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       loading="lazy"
+      className="animate-fade-up animate-duration-1000 animate-delay-1000 animate-ease-in-out"
     />
   );
 }
