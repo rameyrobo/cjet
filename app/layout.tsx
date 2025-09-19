f458b0ab95d98e79aabc31e5bc2b4f982f3a33ef
Ramey Robles <rameyrobo@gmail.com>
2025-09-19 04:48:07 -0700

    Added .ico to browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "styles/tailwind.css"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head><link rel="icon" href="/assets/cjet.ico" type="image/x-icon" /></head>
       <body>{children}</body>
     </html>
   )
