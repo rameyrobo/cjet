c9a91f74a61ffb4159441cdded99e389b88ff75d
Ramey Robles <rameyrobo@gmail.com>
2025-09-23 02:00:17 -0700

    Added Vercel analytics to site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/next"
 import "styles/tailwind.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <Analytics />
         <link rel="icon" href="/assets/cjet.ico" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
